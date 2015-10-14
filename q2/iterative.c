afc581d5ea23a66d3a370d5693858c6b54a4fdda
LuisHsu <luis8404272@gmail.com>
2015-10-14 09:43:11 +0800

    Implement q2 iterative
    I have done q2 iterative during the quiz, so I just update
    q2/iterative.c with that answer.

--- q2/iterative.c
+++ q2/iterative.c
@@ -1,6 +1,23 @@
-/* FIXME: Implement! */
+#include <stdio.h>
+#include <string.h>
+#include <limits.h>
 
-int main()
+char smallest_character(char str[],char c)
 {
+
+    for(unsigned int i=0; str[i]!='\0'; ++i) {
+        if(str[i]>c) {
+            return str[i];
+        }
+    }
+    return c;
+
+}
+
+int main(int argc, char *argv[])
+{
+
+    printf("%c\n",smallest_character(argv[1],argv[2][0]));
+
     return 0;
 }
