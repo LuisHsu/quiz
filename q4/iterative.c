86db996158246abe3b09a40fb5fd4fdd5c261d7d
LuisHsu <luis8404272@gmail.com>
2015-10-17 04:24:04 +0800

    Q4 iterative
    Using the single loop to optimize brute-force solution
    The pros is that it is done in linear time

--- q4/iterative.c
+++ q4/iterative.c
@@ -1,6 +1,34 @@
-/* FIXME: Implement! */
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
 
-int main()
+int maxSubArray(int A[],int n)
 {
+    int max=INT_MIN;
+    int cons=0;
+    for(int i=1; i<n; ++i) {
+        int sum=A[i]+A[i-1];
+        int max_plus_a = cons ? (max+A[i]) : INT_MIN;
+        if(sum>max) {
+            max=sum;
+            cons=1;
+        } else {
+            cons=0;
+        }
+        if(max_plus_a>max) {
+            max=max_plus_a;
+            cons=1;
+        }
+    }
+    return max;
+}
+
+int main(int argc,char *argv[])
+{
+    int array[argc-1];
+    for(int i=0; i<argc-1; ++i) {
+        array[i]=atoi(argv[i+1]);
+    }
+    printf("%d\n",maxSubArray(array,9));
     return 0;
 }
