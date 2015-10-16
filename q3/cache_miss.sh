ce6435267624d499705b78172efb829687901990
LuisHsu <luis8404272@gmail.com>
2015-10-16 18:16:04 +0800

    Test cache-misses
    Put q2's cache-miss test scripts in q3 without modify
    Thanks to the idea of using command-line argunment!!!

---
+++ q3/cache_miss.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+./generator $1
+correct=0
+incorrect=0
+count=1
+rm miss.txt
+for line in `cat "test.txt" | awk {'print $1 ";" $2 ";" $3'}`;do
+	echo "Start test ${count} ..."
+	data=`echo ${line} | cut -d ';' -f 1`
+	char=`echo ${line} | cut -d ';' -f 2`
+	check=`echo ${line} | cut -d ';' -f 3`
+	echo "echo 1 > /proc/sys/vm/drop_caches" | sudo sh
+	res=`perf stat -e cache-misses -o outp2 ./$2 ${data} ${char}` 
+	miss=`cat outp2 | grep 'cache-misses' | awk {'print $1'}`
+	if [ ${res} == ${check} ];then
+		correct=$(( correct + 1 ))
+	else
+		incorrect=$(( incorrect + 1 ))
+	fi
+	echo ${miss//,/} >> miss.txt 
+	echo "Test ${count} cache-miss : ${miss}"
+	count=$(( count + 1 ))
+done
+rm outp*
+rm test.txt
